cc105ad72f96d441745a9ac7f70f8e3e30648683
user <byhoon3@icloud.com>
2023-11-24 16:06:08 +0900

    -

--- naver/views.py
+++ naver/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from bookieum import models
-from bookieum.models import *
 
 import requests, json
 import datetime as dt
