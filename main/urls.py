e09e8780e4f38555b629d37dd94d5d9f301171ff
user <byhoon3@icloud.com>
2023-11-24 23:56:40 +0900

    main 주석처리

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
-    path('recommendation/', views.recommendation),
-]
+# urlpatterns = [
+#     path('recommendation/', views.recommendation),
+# ]
