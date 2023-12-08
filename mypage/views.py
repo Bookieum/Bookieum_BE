e384632229fecae1692d4020215ef41b794bdca7
user <byhoon3@icloud.com>
2023-12-08 23:13:54 +0900

    마이페이지 수정

--- mypage/views.py
+++ mypage/views.py
@@ -44,83 +44,17 @@ def user_information(request):
     user_list=model_to_dict(user_list)
     print(user_list)
 
-    # history
-    
-    check_user_id = get_object_or_404(models.Users, access_token=access_token)
-    h_user_id=check_user_id.user_id
-    
-    b=models.Recommend.objects.select_related('user').filter(user_id=h_user_id)
-    print(b)
-    
-    recommend_id_list=[]
-    for i in b:
-        recommend_id_list.append(i.recommend_id)
-        
-    c=models.RecommendBooks.objects.select_related('recommend__user').filter(recommend_id__in=recommend_id_list)
-    print(c)
-    c_is_selected=c.filter(is_selected=1)
-    print(c_is_selected)  
-    
-    c_is_selected_isbn=[]
-    for i in c_is_selected:
-        c_is_selected_isbn.append(i.isbn_id)
-    print(c_is_selected_isbn)
-    
-    #d=models.RecommendBooks.objects.filter(isbn_id__in=c_is_selected_isbn).prefetch_related('isbn')
-    d=models.Books.objects.filter(isbn_id__in=c_is_selected_isbn)
-    print(d)
-    
-    # fe 에서 필요한 정보 더 추가가능!
-    history=[]
-    for i in d:
-        history.append({'isbn_id':i.isbn_id,'title':i.title,'cover':i.cover})
-    print(history)
-        
-    """
-    # history
-    check_user_id = get_object_or_404(models.Users, access_token=access_token)
-    h_user_id=check_user_id.user_id
 
-    # 테스트용 Recommend, Recommend_books
-    #models.Recommend.objects.create(recommend_id=201,user=check_user_id,recommend_datetime='2023-12-06 17:00:00',emotion=0.5,answer_content='안뇽')    
+    # history
+    user = models.Users.objects.get(access_token=access_token)
+    books = models.RecommendBooks.objects.filter(user=user).select_related('isbn').exclude(is_selected=0).order_by('-created_datetime').all()
+    history = []
+    for book in books:
+        history.append({"mybook_id": book.mybook_id, "recommend_id": book.recommend.recommend_id, "curr_page": book.curr_page, "created_datetime": book.created_datetime,
+                             "is_selected": book.is_selected, "title": book.isbn.title, "cover": book.isbn.cover})
     
-    print(check_user_id.user_name)
-    
-    recommend_list=models.Recommend.objects.filter(user_id=h_user_id)
-    print(recommend_list)
-    
-    recommend_id_info=[]
-    for i in range(len(recommend_list)):
-        recommend_id_info.append(recommend_list[i].recommend_id)
-    
-    recommend_books_info=[]
-    for i in recommend_id_info:
-        recommend_books_check=get_object_or_404(models.RecommendBooks, recommend_id=i)
-        if recommend_books_check.is_selected==1:
-            recommend_books_info.append(recommend_books_check)
-        
-    book_info=[]
-    for i in recommend_books_info:
-        k=get_object_or_404(models.RecommendBooks,recommend_id=i.recommend_id)
-        book_info.append(k.isbn_id)
-    
-    print(book_info)
-    
-    book_list=models.Books.objects.filter(isbn_id__in=book_info)
-    print(book_list)
-    
-    books_list=[]
-    for i in book_list:
-        books_list.append(model_to_dict(i))
-    
-    print(books_list)
-    """
-    
-    
-    
-    #return JsonResponse({'message':'successfully'})
     return JsonResponse({'message': 'successfully', 'data':user_list,'history':history })  
-    #return JsonResponse({'message': 'successfully', 'data':result })  
+ 
     
     
     
